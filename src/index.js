8dbc6a331ab97b01114227abdfa1154709144dc9
jsalcedoe <julio_salcedo2@hotmail.com>
2022-11-02 06:45:59 -0500

    Clase 2022/10/24

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import App from './App';
 import * as serviceWorker from "./serviceWorker";
 //import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
