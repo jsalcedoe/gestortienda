a492ea41f29a33c18021d7cb65db4822a17fe5b5
jsalcedoe <julio_salcedo2@hotmail.com>
2022-11-02 00:51:12 -0500

    Modificaciones Clase 24 de octubre

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Link, Router, Switch } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
