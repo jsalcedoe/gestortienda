493827e57c1090cc0195a1b51397226d15f0129f
jsalcedoe <julio_salcedo2@hotmail.com>
2022-11-13 13:09:45 -0500

    Creacion de componentes de formulario

--- src/components/crear-articulo.js
+++ src/components/crear-articulo.js
@@ -1,17 +1,22 @@
-import React, {components} from "react";
+ import React, {components} from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
+import { FormGroup } from "react-bootstrap";
 export default class CrearArticulos extends components{
     constructor(props){
         super(props);
         this.onChangeCodigo = this.onChangeCodigo.bind(this);
         this.onChageArticuloName = this.onChageArticuloName.bind(this);
+        this.onChageArticuloLinea = this.onChageArticuloLinea.bind(this);
+        this.onChageArticuloProveedor = this.onChageArticuloProveedor.bind(this);
         this.onSubmin = this.onSubmin.bind(this);
 
         this.state = {
             codigo: "",
             name: "",
+            linea:"",
+            proveedor:"",
         }; 
     }
     onChageArticuloCodigo(e){
@@ -21,11 +26,19 @@ export default class CrearArticulos extends components{
         this.setState({name: target.value})
 
     }
+    onChageArticuloLinea(e){
+        this.setState({linea: target.value})
+    }
+    onChageArticuloProveedor(e){
+        this.setState({proveedor: target.value})
+    }
     onSubmin(e){
         e.preventDefault();
         const articuloObject = {
             codigo:this.state.codigo,
             name: this.state.name,
+            linea: this.state.linea,
+            proveedor: this.state.proveedor,
         };
         axios
             .post("http://localhost:4000/articulo/create-articulo",articuloObject)
@@ -33,6 +46,48 @@ export default class CrearArticulos extends components{
         this.setState({codigo:"", name:""});    
     }
     render(){
-        return <div class = 'form-wrapper'>crear-articulo.components</div>
+        return <div class = 'form-wrapper'>crear-articulo.components
+            <form>
+                <Form.Group controlId ="codigo">
+                    <Form.label>Codigo del Articulo</Form.label>
+                    <Form.Control 
+                        type = "text"
+                        value = {this.state.codigo}
+                        onChange = {this.onChageArticuloCodigo}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId ="name">
+                    <Form.label>Nombre del Articulo</Form.label>
+                    <Form.Control 
+                        type = "text"
+                        value = {this.state.name}
+                        onChange = {this.onChageArticuloName}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId ="linea">
+                    <Form.label>Linea del Articulo</Form.label>
+                    <Form.Control 
+                        type = "text"
+                        value = {this.state.linea}
+                        onChange = {this.onChageArticuloLinea}
+                    />
+                </Form.Group>
+
+                <Form.Group controlId ="proveedor">
+                    <Form.label>proveedor del Articulo</Form.label>
+                    <Form.Control 
+                        type = "text"
+                        value = {this.state.proveedor}
+                        onChange = {this.onChageArticuloProveedor}
+                    />
+                </Form.Group>
+
+                <Button variant = "danger" size = "lg" block = "block" type = "submit" classname = "mt-4">
+                    Crecion de articulo
+                </Button>
+            </form>
+        </div>
     }
 }
